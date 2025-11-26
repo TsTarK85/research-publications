faa417ba3d9e40ee5b09ffc17d3bb4907b63cdec
TsTarK85 <aditya1612singh@gmail.com>
2025-11-26 19:34:45 +0530

    hardcoded key removed

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ffnaodgwzdzcaedhqyqh.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmbmFvZGd3emR6Y2FlZGhxeXFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNzc3NDEsImV4cCI6MjA3OTY1Mzc0MX0.7KyVwB3qqkC-dTmy5v-6blZyWyBYf95y1-hlMyDG_ps";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
